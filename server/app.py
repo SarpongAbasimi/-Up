0e61eacaaebd8b9f61f27861ff4cb939ef83902c
sarpong <abasimisarpong@rocketmail.com>
2021-03-28 02:15:20 +0100

    [SA] How to run a flask react app with docker

---
+++ server/app.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/api', methods=['GET'])
+def index():
+  return {
+    "channel":"The Show",
+    "tutorial": "React, Flask and Docker."
+    }
+
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0')
